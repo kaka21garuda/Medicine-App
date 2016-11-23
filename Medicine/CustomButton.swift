75bd155e768c3be622cda439a54ce1f5f71d3a60
Buka Cakrawala <kaka10garuda@gmail.com>
2016-11-23 11:32:19 -0800

    Custom Button class to modify button

---
+++ Medicine/CustomButton.swift
@@ -0,0 +1,45 @@
+//
+//  CustomButton.swift
+//  Medicine
+//
+//  Created by Buka Cakrawala on 11/23/16.
+//  Copyright © 2016 Buka Cakrawala. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CustomButton: UIButton {
+    
+    @IBInspectable var borderColor: UIColor? = UIColor.clear {
+        didSet {
+            layer.borderColor = self.borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = self.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = self.cornerRadius
+            layer.masksToBounds = self.cornerRadius > 0
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        self.layer.cornerRadius = self.cornerRadius
+        self.layer.borderWidth = self.borderWidth
+        self.layer.borderColor = self.borderColor?.cgColor
+    }
+}
