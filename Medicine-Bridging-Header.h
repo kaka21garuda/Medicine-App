3ae0d83c66a77d5cfa5b9a9c29e0f06d15b88dbe
Buka Cakrawala <kaka10garuda@gmail.com>
2016-11-23 18:58:21 -0800

    setup BAFluidView framework

---
+++ Medicine-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Medicine-Bridging-Header.h
+//  Medicine
+//
+//  Created by Buka Cakrawala on 11/23/16.
+//  Copyright © 2016 Buka Cakrawala. All rights reserved.
+//
+
+#ifndef Medicine_Bridging_Header_h
+#define Medicine_Bridging_Header_h
+
+
+#endif /* Medicine_Bridging_Header_h */
+#import "BAFluidView.h"
